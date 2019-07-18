586e4de0c0a8d768c17d0ea0647a8d0bccd28ff6
abidex4yemi <abidex4yemi@gmail.com>
2019-07-18 13:57:54 +0100

    Seed initial values into recipes table

--- src/db/migrations/20190718125640_recipes-schema.js
+++ src/db/migrations/20190718125640_recipes-schema.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable func-names */
 exports.up = function (knex, Promise) {
-  return knex.schema.createTable('recipes', table => table
-    .increments()
-    .string('name')
-    .unique()
-    .notNullable());
+  return knex.schema.createTable('recipes', (table) => {
+    table.increments();
+    table
+      .string('name')
+      .unique()
+      .notNullable();
+  });
 };
 
 exports.down = function (knex, Promise) {
