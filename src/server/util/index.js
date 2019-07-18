a94bf59570e9c5b609e6d8f2f485483a6896093c
abidex4yemi <abidex4yemi@gmail.com>
2019-07-18 20:12:15 +0100

    Create error helper functions

---
+++ src/server/util/index.js
@@ -0,0 +1,6 @@
+// Export all utility files
+export {
+  createError, BAD_REQUEST, CONFLICT, NOT_FOUND, GENERIC_ERROR,
+} from './error';
+export { OK, CREATED, createSuccess } from './success';
+export { default as joiValidate } from './joiValidate';
